e0c02992d5628c5c27819ff8aabb2a27dd8612d4
Shreyas Mayya <shreyas@smayya.me>
2026-03-21 23:20:29 -0400

    comment out error handling

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,20 +1,21 @@
 from deepface import DeepFace
+import json
 import os
 import sys
 
-def verify_face_not_in_excludes(face_path: str, exclude_path: str) -> bool:
+def verify_face_not_in_excludes(face_path: str = sys.argv[1], exclude_path: str = "faces_to_ignore") -> bool:
     for exclude_face in os.listdir(exclude_path):
         exclude_face_path = os.path.join(exclude_path, exclude_face)
-        try:
-            if DeepFace.verify(face_path, exclude_face_path, model_name='ArcFace', detector_backend='opencv')["verified"]:
-                return False
-        except Exception:
-            return False  # If there's an error (e.g., no face detected), we treat it as a suspicious case and return False
+        # try:
+        if DeepFace.verify(face_path, exclude_face_path, model_name='ArcFace', detector_backend='opencv')["verified"]:
+            return False
+        # except Exception:
+        #     return True  # If there's an error (e.g., no face detected), we treat it as a suspicious case and return True
     return True
 
 image_to_check = sys.argv[1]
 
 if verify_face_not_in_excludes(image_to_check, "faces_to_ignore"):
-    print("This is not a face we want to ignore.")
+    print("This face is suspicious.")
 else:
-    print("This is a face we want to ignore.")
+    print("We know this person.")
